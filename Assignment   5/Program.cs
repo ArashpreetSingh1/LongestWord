e8666b20742fe386bceebc9615d02e0f128eb05b
visualstudio <visualstudio@122-PC32>
2019-04-17 09:42:25 -0400

    02

--- Assignment   5/Program.cs
+++ Assignment   5/Program.cs
@@ -10,6 +10,36 @@ namespace Assignment___5
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest word is : ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first alogrithm designed for processing by an Analytical Engine."));
+        }
+        public MyFunctionA(string input)
+        {
+            //Write a C# program to find the longest word in a string.
+            string[] words = input.Split('');
+            int wordArrayLength = words.Length;
+            int[] wordsLength = new int[wordArrayLength];
+            int x = 0;
+            foreach (var word in words)
+            {
+                //TODO
+            }
+            //loop post condition: we now have array wordsLength which
+            // contains the lengths of each word
+
+            string currentWord = words[0];
+            string nextWord;
+            stringlongestWord = currentWord;
+
+            for (int y = 0;y<words.Length-1;y++)
+            {
+                currentWord = words[y];
+                nestWord = words[y + 1];
+                //TODO: Use an IFStatement to make sure that variable longestWord
+                // is always set to the Longest Word in the input string
+            }
+        }
         }
     }
-}
+
